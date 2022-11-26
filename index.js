8d36956dfc30966201a9bade47c8a428677928ef
Alexa <kusteralexa@gmail.com>
2022-11-25 23:14:06 -0500

    commit tree c2904e70734bcaed8ab572d0d8b2605a852e4871

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = Number(num1 * num2)
+
+
+// let random = Number.random();
+let random = 1;
+
+let num3 = 4;
+let num4 = 34;
+
+let mod = Number(num3 % num4)
+
+let max = 20;
